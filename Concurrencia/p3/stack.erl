010fa91439e37536305e87a9854f44bb6df6d58f
Tiago <tiagovolta2@gmail.com>
2025-03-29 10:34:11 +0100

    Update stack.erl

--- Concurrencia/p3/stack.erl
+++ Concurrencia/p3/stack.erl
@@ -1,1 +1,21 @@
+-module(stack).
 
+-export([empty/0, push/2, pop/1, peek/1]).
+
+% Creates an empty stack
+empty() -> [].
+
+% Adds one element to the stack
+push(Stack, Elem) -> [Elem | Stack].
+
+% Deletes the peek element
+pop([]) ->
+    [];
+pop([_ | Rest]) ->
+    Rest.
+
+% Returns the tuple {ok, Elem} where Elem is the peek element
+peek([]) ->
+    empty;
+peek([Top | _]) ->
+     {ok, Top}.
